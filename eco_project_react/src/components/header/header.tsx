841aee37b06c680f7cc4ea17b551d037f0bd2e31
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-02-17 20:46:12 +0300

    eco-project-react

---
+++ eco_project_react/src/components/header/header.tsx
@@ -0,0 +1,44 @@
+import logo from "../../logo.svg";
+import React from "react";
+import styles from "./header.module.sass";
+
+function Header() {
+    return (
+            <div className={styles.header__wrapper}>
+                <header className={styles.header}>
+                    <nav className={styles.navPanel}>
+                        <div className={styles.header__logo}>
+                            <img src="assets/images/ecorusLogo.png" alt="EcoRusLogo"/>
+                        </div>
+                        <ul className={styles.navList}>
+                            <li className={styles.navLink}>
+                                Главная
+                            </li>
+                            <li className={styles.navLink}>
+                                Пункты сбора
+                            </li>
+                            <li className={styles.navLink}>
+                                ЭкоМаркет
+                            </li>
+                            <li className={styles.navLink}>
+                                О сервисе
+                            </li>
+                        </ul>
+                    </nav>
+                    <ul className={styles.navList}>
+                        <li className={styles.navLink}>
+                            <img src="assets/images/view-box.png" className={styles.header__icon}/>
+                                Казань
+                        </li>
+                        <li className={styles.navLink}>
+                            <img src="assets/images/Union.png" className={styles.header__icon}/>
+                                Войти
+                        </li>
+                    </ul>
+                </header>
+            </div>
+    );
+}
+
+export default Header;
+
